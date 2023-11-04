72c7fa976ada1783690ef79ffedc930400e7c1a5
jack <ysj@rmsoft.kr>
2023-11-04 05:40:03 +0000

    1104

--- utils/filter_audio_from_nas.py
+++ utils/filter_audio_from_nas.py
@@ -3,23 +3,31 @@ import csv
 import shutil
 import argparse
 from tqdm import tqdm
+
 def main(args):
     for _root, _dirs, files in os.walk(args.tsv_files):
         if files:
             for file in files:
                 file_name, ext = os.path.splitext(file)
                 if ext == ".tsv" and "refined" not in file_name:
-                    print(os.path.join(_root, _dirs, file))
+                    print(os.path.join(_root, file))
                     cur_tsv_path = os.path.abspath(file)
                     with open(cur_tsv_path, "r+", encoding = "utf-8") as f:
                         cur_file = csv.reader(f, delimiter = "\n")
-                        for row in tqdm(cur_file):
+                        progress_bar = tqdm(cur_file, desc=f"copying...")
+                        for row in progress_bar:
                             path, transcription = row[0].split(" :: ")
                             if os.path.exists(path):
                                 source = os.path.join(args.root)
+                                destination = os.path.join(arg.destination, path)
+                                dst_folder = os.path.dirname(destination)
+                                if not os.path.exists(dst_folder):
+                                    os.makedirs(dst_folder)
+                                copyfile(source, destination)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_arguments("--tsv_files", help="directory path to the tsv files.")
+    parser.add_arguments("--destination", help="destination of the folder path.")
     args = parser.parse_args()
     main(args)
