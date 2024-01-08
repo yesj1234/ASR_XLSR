9c9e4c87f85091ada55c980b68b94c8ab0895b84
jack <ysj@rmsoft.kr>
2024-01-08 07:25:01 +0000

    add utils for sup version data

---
+++ utils/gen_tsv_sup.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+# Change the source lang accordingly. ko / en / ja / zh
+export SOURCE_LANG=en
+
+#Generate tsv files from json 
+python3 from_json.py --jsons /home/ubuntu/3.보완조치완료/1.Training/ --dest /home/ubuntu/asr_split/train.tsv
+# python3 from_json.py --jsons /home/ubuntu/3.보완조치완료/2.Test/ --dest /home/ubuntu/asr_split/test.tsv
+# python3 from_json.py --jsons /home/ubuntu/3.보완조치완료/3.Validation/ --dest /home/ubuntu/asr_split/validation.tsv
+
+#refine tsv files 
+python3 refine_data.py --tsv_splits_dir /home/ubuntu/asr_split --lang $SOURCE_LANG
